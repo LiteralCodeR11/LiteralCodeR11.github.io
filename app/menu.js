c24b8d76e0e52e35bb4a8518a7551d4e17a40557
Juan Alvaro Martinez <juanalvaromartinez@gmail.com>
2022-09-21 08:37:17 -0430

    se agregó las redes sociales al proyecto code r11

--- app/menu.js
+++ app/menu.js
@@ -7,18 +7,36 @@ function crearEncabezado() {
     </a>
 
     <nav>
-        <ul>
-            <li><a href="index.html">Inicio</a></li>
-            <li><a href="proyecto-code-r11.html">Code R11</a></li>
-            <li><a href="Blog.html">Blog</a></li>
-        </ul>
+      <div class="redes-sociales">
+        <a href="https://twitter.com/LiteralCodeR11" target="_blank">
+            <figure>
+                <img src="/public/images/redes-sociales/twitter-svgrepo-com.svg" alt="Logo Twitter">
+            </figure>
+        </a>
+        <a href="https://www.facebook.com/LiteralCodeR11" target="_blank">
+            <figure>
+                <img src="/public/images/redes-sociales/facebook-svgrepo-com.svg" alt="Logo Facebook">
+            </figure>
+        </a>
+        <a href="https://github.com/LiteralCodeR11" target="_blank">
+            <figure>
+                <img src="/public/images/redes-sociales/github-svgrepo-com.svg" alt="Logo GitHub">
+            </figure>
+        </a>
+      </div>
+      <ul>
+          <li><a href="index.html">Inicio</a></li>
+          <li><a href="proyecto-code-r11.html">Code R11</a></li>
+          <li><a href="Blog.html">Blog</a></li>
+      </ul>
     </nav>`;
 }
 
 function crearPiePagina() {
   return `
-        <p>Design by Juan Alvaro Martinez | 2022 Equipo Code R11 | <a href="https://github.com/LiteralCodeR11/LiteralCodeR11.github.io" target="_blank" class="links-footer">GitHub</a></p>
-        <p>Aprendizaje Activo basado en Proyectos</p>
+        <p>Design by Juan Alvaro Martinez | 2022 Equipo Code R11 </p>         
+        <p>Aprendizaje Activo basado en Proyectos | <a href="https://github.com/LiteralCodeR11/LiteralCodeR11.github.io" target="_blank" class="links-footer">GitHub</a></p>
+        <a href="mailto:literalcoder11@gmail.com">literalcoder11@gmail.com</a> 
         `;
 }
 
