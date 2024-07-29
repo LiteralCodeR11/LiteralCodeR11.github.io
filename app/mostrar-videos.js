4d96bd35c7de867199df884a390591d1a3ddafb4
CodeSJMartinez <serviciosjmartinez@gmail.com>
2024-07-29 21:23:40 +0200

    Mejora de la función crearLista: validación de entradas y optimización de accesos al DOM
    - Añadidas validaciones para asegurar que el objeto de entrada no está vacío y que contiene la propiedad esperada 'grupo_videos'.
    - Se incluye un manejo de errores que imprime un mensaje en la consola si el objeto de entrada no tiene el formato esperado.
    - Optimización del acceso al elemento del DOM 'Todos' moviéndolo fuera del bucle 'map', mejorando la eficiencia.

--- app/mostrar-videos.js
+++ app/mostrar-videos.js
@@ -1,32 +1,29 @@
 import { listaVideos } from "./dataBase/dbVideos.js";
 
 function crearLista(obj) {
-  return Object.values(obj[0].grupo_videos).map(
-    (registro) => `
-      <div class="ficha-video-item">
-        <figure>
-          <img onclick='crearVideos("https://www.youtube.com/embed/${
-            registro.idVideo
-          }")'
-              src="https://i.ytimg.com/vi/${registro.idVideo}/${
-      registro.imagen
-    }.jpg" 
-              alt="${registro.titulo}"  
-              width="${registro.imgAncho}" height="${registro.imgAlto}">
-      </figure>
-      
-      <div class="textos">
-          <h2>${registro.titulo}</h2>
-          <h3>${registro.canal}</h3>
-          <p style="display:none">${registro.texto}</p>        
-          <h3 class="todos">${
-            document.getElementById("Todos") ? registro.categoria : ""
-          }</h3>              
-      </div>
+  if (!Array.isArray(obj) || obj.length === 0 || !obj[0].grupo_videos) {
+    console.error("El objeto no tiene el formato esperado.");
+    return "";
+  }
   
-  </div>
-      `
-  ); 
+  const todosElement = document.getElementById("Todos");
+  
+  return Object.values(obj[0].grupo_videos).map((registro) => `
+    <div class="ficha-video-item">
+      <figure>
+        <img onclick='crearVideos("https://www.youtube.com/embed/${registro.idVideo}")'
+             src="https://i.ytimg.com/vi/${registro.idVideo}/${registro.imagen}.jpg"
+             alt="${registro.titulo}"
+             width="${registro.imgAncho}" height="${registro.imgAlto}">
+      </figure>
+      <div class="textos">
+        <h2>${registro.titulo}</h2>
+        <h3>${registro.canal}</h3>
+        <p style="display:none">${registro.texto}</p>
+        <h3 class="todos">${todosElement ? registro.categoria : ""}</h3>
+      </div>
+    </div>
+  `).join("");
 }
 
 function mostrarVideos() {
